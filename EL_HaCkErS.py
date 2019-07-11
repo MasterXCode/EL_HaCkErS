77894ee7b35ce64803e1c7d3db631d1c2aeed653
MASTERX <50529518+seifsherifssss@users.noreply.github.com>
2019-07-11 14:53:36 +0200

    Add files via upload

---
+++ EL_HaCkErS.py
@@ -0,0 +1,32 @@
+def el () :
+    import os
+    os.system("clear && cowthink -f meow 'Welcome To EL_HaCkErS' | lolcat " ) 
+    import os 
+    os.system ( " echo ' ' " ) 
+    import os 
+    os.system ( " toilet -f big 'EL_HaCkErS' | lolcat ")
+    import os 
+    os.system ( " echo ' ' " ) 
+    import os 
+    os.system ( " toilet -f small 'version 1 ' | lolcat ")
+    print ("1)Phishing ")
+    print ("2)HackingPhone")
+    print ("3)HackingWebsites")
+    print ("4)MasterMarket")
+    import os 
+    os.system ( " echo ' ' " ) 
+    look=input (" =======>>》  ")
+    if look== '1' :
+        import Phish
+    if look== '2' :
+        import Hack
+    if look== '3' :
+        import HackWebsites
+    if look== '4' :
+        import MasterMarket
+    else : 
+        print ( " Please Type a Number ")
+        import os 
+        os.system("sleep2")
+        el()
+el()
---
+++ requirements.sh
@@ -0,0 +1,46 @@
+pkg install python figlet  axel toilet git ruby perl -y && gem install lolcat && pkg install fish 
+
+pkg install toilet figlet curl php ruby nmap openssh openssl-tool nodejs ruby cowsay perl fish dnsutils termux-exec net-tools -y && pkg upgrade -y && gem install lolcat 
+
+
+
+pkg install php openssh python curl openssl-tool nmap ruby perl toilet figlet fish cowsay tree -y && gem install lolcat && pkg upgrade -y && pkg install net-tools hydra -y
+
+
+clear
+
+touch /$PREFIX/bin/EL_HaCkErS 
+echo ' python /$HOME/EL_HaCkErS/EL_HaCkErS.py ' > /$PREFIX/bin/EL_HaCkErS 
+
+clear
+echo "########################"
+echo ""
+echo "     RUNNING      "
+echo ""
+echo "########################"
+echo ""
+echo " Updating && Upgrading termux "
+echo ""
+pkg update && pkg upgrade -y 
+echo "" 
+echo "%%%%%%%%%%%%%%%%%%"
+echo ""
+echo " Installing Requirements "
+echo ""
+echo "%%%%%%%%%%%%%%%%%%"
+pkg install python cowsay figlet toilet git ruby -y
+echo "" 
+echo "&&&&&&&&&&&&&&&&&&&&&&&"
+echo ""
+toilet "     Installing   "
+echo "" 
+echo "&&&&&&&&&&&&&&&&&&&&&&&"
+gem install lolcat 
+echo ""
+clear 
+toilet -f big " DONE....!" -F gay | lolcat
+echo ""
+echo " Now you can Launch it by just Typing ====> EL_HaCkErS in your terminal "
+
+toilet -f small ' BYE BYE ' | lolcat 
+
